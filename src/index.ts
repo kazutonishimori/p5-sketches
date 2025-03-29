b43ced4407ec3b67690da39d7c998604c2f48b6b
kazutonishimori <92814675+kazutonishimori@users.noreply.github.com>
2025-03-29 00:21:48 -0400

    v1

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,44 @@ init(p5);
 let sketch = (s: p5SVG) => {
     s.setup = () => {
         s.createCanvas(window.innerWidth, window.innerHeight)
+        dotGrid();
     }
 
     s.draw = () => {
         s.background(220);
-        s.textSize(50);
-        s.text('hello world', 10, 50);
+        time += 0.01;
+        dotGrid();
+        drawText();
+    }
+    let xScale = 0.015;
+    let yScale = 0.02;
+    let gap = 5;
+    let time = 0; // Initialize the time variable
+
+    function dotGrid() {
+        s.noStroke();
+        s.fill(0);
+
+        // Loop through x and y coordinates, at increments set by gap
+        for (let x = gap / 2; x < s.width; x += gap) {
+            for (let y = gap / 2; y < s.height; y += gap) {
+                let noiseValue = s.noise((x + s.mouseX) * xScale, (y + s.mouseY) * yScale, time); // Add 'time' as the third argument
+                let diameter = noiseValue * gap;
+                let colorValue = noiseValue * 255; // Dynamic color based on noise value
+                s.fill(255-colorValue, colorValue, colorValue);
+                s.circle(x, y, diameter);
+            }
+        }
+    }
+
+    function drawText() {
+        let offsetX = ( s.width / 2) - ((s.mouseX - s.width / 2) * 0.8);
+        let offsetY = ( s.height / 2) - ((s.mouseY - s.height / 2) * 0.8) ;
+        s.fill(0);
+        s.textSize(32);
+        s.text('Welcome!', offsetX, offsetY);
+        s.text('about me', offsetX + 400, offsetY - 300);
+        s.text('lalala', offsetX + s.width/1.5, offsetY - s.height/1.5);
     }
 
     s.keyPressed = () => {
